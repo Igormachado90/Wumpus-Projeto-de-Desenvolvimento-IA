fcb47d1c920dae7c589fc10c2c48df900f250fe2
Igormachado90 <igorwenceslau90@gmail.com>
2026-09-02 22:10:41 -0300

    02/09

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Wumpus-Projeto-de-Desenvolvimento-IA/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    assetsDir: 'assets',
+    copyPublicDir: true,
+  },
+  publicDir: 'public',
 })
